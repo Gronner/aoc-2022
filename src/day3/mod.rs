16a175e4a8c376b2d0f13b4d864136b2fb099e4c
Gronner <felix.braeunling@t-online.de>
2022-12-03 06:35:12 +0100

    Add Gronners day 3 solution

---
+++ src/day3/mod.rs
@@ -0,0 +1,85 @@
+use aoc_downloader::download_day;
+use std::collections::HashSet;
+use std::iter::FromIterator;
+
+const DAY: u32 = 3;
+
+fn get_input() -> Vec<String> {
+    use std::io::BufRead;
+    download_day((DAY) as u32, "input").unwrap();
+
+    let file = std::fs::File::open(format!("input/input{}.txt", DAY)).unwrap();
+    let reader = std::io::BufReader::new(&file);
+    reader.lines().collect::<Result<_, _>>().unwrap()
+}
+
+fn parse_input(input: Vec<String>) -> Vec<Vec<char>> {
+    input
+        .iter()
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .collect::<Vec<_>>()
+}
+
+pub fn run_day() {
+    let input = get_input();
+    let input = parse_input(input);
+    println!("Running day {}:\n\tPart1 {}\n\tPart2 {}", DAY, part1(&input), part2(&input));
+}
+
+fn part1(input: &Vec<Vec<char>>) -> u32 {
+    let mut priority = 0;
+    for backpack in input {
+        let bagsize = backpack.len();
+        let left_compartment: HashSet<&char> = HashSet::from_iter(&backpack[..bagsize/2]);
+        let right_compartment = HashSet::from_iter(&backpack[bagsize/2..]);
+        let in_common = left_compartment.intersection(&right_compartment);
+        priority += in_common.into_iter()
+            .map(|&&item| {
+                if item.is_lowercase() {
+                    item as u32 - ('a' as u32) + 1
+                } else {
+                    item as u32 - ('A' as u32) + 27
+                }
+            })
+            .sum::<u32>();
+    }
+    priority
+}
+
+fn part2(input: &Vec<Vec<char>>) -> u32 {
+    let mut priority = 0;
+    for backpack in input.chunks(3) {
+        let elf1: HashSet<char> = HashSet::from_iter(backpack[0].clone());
+        let elf2: HashSet<char> = HashSet::from_iter(backpack[1].clone());
+        let elf3: HashSet<char> = HashSet::from_iter(backpack[2].clone());
+        let in_common = elf1.intersection(&elf2).cloned().collect::<HashSet<_>>();
+        let in_common = in_common.intersection(&elf3);
+        priority += in_common.into_iter()
+            .map(|&item| {
+                if item.is_lowercase() {
+                    item as u32 - ('a' as u32) + 1
+                } else {
+                    item as u32 - ('A' as u32) + 27
+                }
+            })
+            .sum::<u32>();
+    }
+    priority
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day0_part1_output() {
+        let input = parse_input(get_input());
+        assert_eq!(744475, part1(&input));
+    }
+
+    #[test]
+    fn day0_part2_output() {
+        let input = parse_input(get_input());
+        assert_eq!(70276940, part2(&input));
+    }
+}
