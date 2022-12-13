ea3f00a72e37f5491a101857b7060c137910d8a6
Gronner <felix.braeunling@t-online.de>
2022-12-13 07:33:54 +0100

    Refactor day 13 solution

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -28,6 +28,7 @@ fn parse_input(input: Vec<String>) -> Vec<Input> {
         .collect::<Vec<_>>()
 }
 
+#[allow(clippy::derive_ord_xor_partial_ord)]
 #[derive(Debug, Ord, Eq, Clone)]
 enum Side {
     Integer(u64),
@@ -50,7 +51,7 @@ impl Side {
                 list.push(new_side);
                 advance = new_i + 1;
                 continue;
-            } else if c.is_digit(10) {
+            } else if c.is_ascii_digit() {
                 stack.push(c);
                 continue;
             } else if c == ',' && !stack.is_empty() {
@@ -64,10 +65,10 @@ impl Side {
                 }
                 return Ok((Side::List(list), i));
             } else {
-                panic!("Unknown char: {}", c);
+                panic!("Unknown char: {c}");
             }
         }
-        return Ok((Side::List(list), 0));
+        Ok((Side::List(list), 0))
     }
 }
 
@@ -82,7 +83,7 @@ impl std::cmp::PartialOrd for Side {
         match(self, other) {
             (Side::Integer(left), Side::Integer(right)) => left.partial_cmp(right),
             (Side::List(_), Side::Integer(_)) => self.partial_cmp(&Side::List(vec![other.clone()])),
-            (Side::Integer(_), Side::List(_)) => Side::List(vec![self.clone()]).partial_cmp(&other),
+            (Side::Integer(_), Side::List(_)) => Side::List(vec![self.clone()]).partial_cmp(other),
             (Side::List(left), Side::List(right)) => {
                 for (list_left, list_right) in left.iter().zip(right) {
                     if let Some(result) = list_left.partial_cmp(list_right) {
@@ -132,12 +133,12 @@ mod tests {
     #[test]
     fn day0_part1_output() {
         let input = parse_input(get_input());
-        assert_eq!(744475, part1(&input));
+        assert_eq!(5852, part1(&input));
     }
 
     #[test]
     fn day0_part2_output() {
         let input = parse_input(get_input());
-        assert_eq!(70276940, part2(&input));
+        assert_eq!(24190, part2(&input));
     }
 }
