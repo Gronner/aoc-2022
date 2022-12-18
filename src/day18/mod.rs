7b84347e5302e1029f5545c1c6e87abe2ca186cd
Gronner <felix.braeunling@t-online.de>
2022-12-18 08:07:23 +0100

    Refactor day 18 solution further

--- src/day18/mod.rs
+++ src/day18/mod.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashSet, HashMap};
+use once_cell::unsync::Lazy;
 
 use aoc_downloader::download_day;
 
@@ -19,7 +20,7 @@ type Output = u64;
 fn parse_input(input: Vec<String>) -> Vec<Input> {
     input
         .iter()
-        .map(|line| line.split(",").map(|v| v.parse::<i64>().unwrap()).collect())
+        .map(|line| line.split(',').map(|v| v.parse::<i64>().unwrap()).collect())
         .collect::<Vec<_>>()
 }
 
@@ -29,29 +30,23 @@ pub fn run_day() {
     println!("Running day {}:\n\tPart1 {}\n\tPart2 {}", DAY, part1(&input), part2(&input));
 }
 
-fn get_neighbours(pos: (i64, i64, i64)) -> Vec<(i64, i64, i64)> {
-    let offsets = vec![
+fn get_offsets() -> Vec<(i64, i64, i64)> {
+    let offsets = Lazy::new(|| {vec![
         (1, 0, 0), (-1, 0, 0),
         (0, 1, 0), (0, -1, 0),
         (0, 0, 1), (0, 0, -1),
-    ];
-    let mut neighbours = vec![];
-    for offset in offsets {
-        neighbours.push((pos.0 + offset.0, pos.1 + offset.1, pos.2 + offset.2));
-    }
-    neighbours
+    ]});
+    offsets.clone()
 }
 
 fn count_neighbours(grid: &HashMap<(i64, i64, i64), u64>, pos: (i64, i64, i64)) -> u64 {
     let mut uncovered = *grid.get(&pos).unwrap();
 
-    for neighbour in get_neighbours(pos) {
-        if grid.contains_key(&neighbour) {
-            uncovered -= 1;
-        }
-
-    }
-
+    uncovered -= get_offsets()
+        .iter()
+        .map(|off| (pos.0 + off.0, pos.1 + off.1, pos.2 + off.2))
+        .filter(|neigh| grid.contains_key(neigh))
+        .count() as u64;
     uncovered
 }
 
@@ -68,13 +63,13 @@ fn part1(input: &[Input]) -> Output {
     sum
 }
 
-fn is_external(external: &mut HashSet<(i64, i64, i64)>, grid: &HashSet<(i64, i64, i64)>, pos: (i64, i64, i64), boundries: (i64, i64, i64, i64, i64, i64)) -> bool {
-    if grid.contains(&pos) {
+fn is_external(external: &mut HashSet<(i64, i64, i64)>, grid: &HashSet<(i64, i64, i64)>, pos: &(i64, i64, i64), boundries: (i64, i64, i64, i64, i64, i64)) -> bool {
+    if grid.contains(pos) {
         return false;
     }
 
     let mut visited: HashSet<(i64, i64, i64)> = HashSet::new();
-    let mut stack = vec![pos];
+    let mut stack = vec![*pos];
 
     while !stack.is_empty() {
         let next = stack.pop().unwrap();
@@ -87,13 +82,15 @@ fn is_external(external: &mut HashSet<(i64, i64, i64)>, grid: &HashSet<(i64, i64
             || !(boundries.0..=boundries.1).any(|v| v == next.0)
             || !(boundries.2..=boundries.3).any(|v| v == next.1)
             || !(boundries.4..=boundries.5).any(|v| v == next.2) {
-            let diff: HashSet<(i64, i64, i64)> = visited.difference(&grid).map(|&pos| pos).collect();
-            external.extend(&diff);
+            external.extend(visited.difference(grid).into_iter());
             return true;
         }
 
         if !grid.contains(&next) {
-            stack.append(&mut get_neighbours(next));
+            stack.extend(
+                get_offsets()
+                .iter()
+                .map(|off| (next.0 + off.0, next.1 + off.1, next.2 + off.2)));
         }
     }
 
@@ -140,17 +137,16 @@ fn part2(input: &[Input]) -> Output {
 
     let mut external = HashSet::new();
 
-    let mut sum = 0;
-
-    for cube in &exposed {
-        for neighbour in get_neighbours(*cube) {
-            if is_external(&mut external, &exposed, neighbour, boundries) {
-                sum += 1;
-            }
-        }
-    }
-
-    sum
+    exposed
+        .iter()
+        .map(|cube| {
+            get_offsets()
+                .iter()
+                .map(|off| (cube.0 + off.0, cube.1 + off.1, cube.2 + off.2))
+                .filter(|neigh| is_external(&mut external, &exposed, neigh, boundries))
+                .count()
+        })
+        .sum::<usize>() as u64
 }
 
 #[cfg(test)]
@@ -160,12 +156,12 @@ mod tests {
     #[test]
     fn day0_part1_output() {
         let input = parse_input(get_input());
-        assert_eq!(744475, part1(&input));
+        assert_eq!(4512, part1(&input));
     }
 
     #[test]
     fn day0_part2_output() {
         let input = parse_input(get_input());
-        assert_eq!(70276940, part2(&input));
+        assert_eq!(2554, part2(&input));
     }
 }
