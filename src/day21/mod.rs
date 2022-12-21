17871c7d5b85f3e29c1a42748bb2343c987a34b9
Gronner <felix.braeunling@t-online.de>
2022-12-21 22:32:21 +0100

    Refactor day 21 solution

--- src/day21/mod.rs
+++ src/day21/mod.rs
@@ -15,12 +15,12 @@ fn get_input() -> Vec<String> {
 }
 
 type Input = HashMap<String, Monkey>;
-type Output = i64;
+type Output = f64;
 
 #[derive(Clone, Debug)]
 enum Monkey {
     Job(String, String, char),
-    Num(i64)
+    Num(f64)
 }
 
 impl FromStr for Monkey {
@@ -30,7 +30,7 @@ impl FromStr for Monkey {
         let num_mon_re = regex!(r"(\d+)");
         let job_mon_re = regex!(r"(\w+) ([\+\-*/=]) (\w+)");
         if let Some(monkey) = num_mon_re.captures(s).map(|captured| {
-            Monkey::Num(captured[1].parse::<i64>().unwrap())
+            Monkey::Num(captured[1].parse::<f64>().unwrap())
         }) {
             return Ok(monkey);
         }
@@ -66,7 +66,7 @@ pub fn run_day() {
     println!("Running day {}:\n\tPart1 {}\n\tPart2 {}", DAY, part1(&input), part2(&input));
 }
 
-fn tree_walk(monkey_tree: &HashMap<String, Monkey>, node: &str) -> i64 {
+fn tree_walk(monkey_tree: &HashMap<String, Monkey>, node: &str) -> f64 {
     if let Some(Monkey::Num(n)) = monkey_tree.get(node) {
         return *n;
     }
@@ -79,9 +79,9 @@ fn tree_walk(monkey_tree: &HashMap<String, Monkey>, node: &str) -> i64 {
             '/' => tree_walk(monkey_tree, left) / tree_walk(monkey_tree, right),
             '=' => {
                 if tree_walk(monkey_tree, left) == tree_walk(monkey_tree, right) {
-                    0
+                    0.0
                 } else {
-                    1
+                    1.0
                 }
             },
             _ => unreachable!("Unkown symbol {op}"),
@@ -100,7 +100,7 @@ fn equation_generator(monkey_tree: &HashMap<String, Monkey>, node: &str) -> Stri
     }
 
     if let Some(Monkey::Job(left, right, op)) = monkey_tree.get(node) {
-        return vec!["(".to_string(), equation_generator(monkey_tree, left), op.to_string(), equation_generator(monkey_tree, right), ")".to_string()].connect(" ");
+        return vec!["(".to_string(), equation_generator(monkey_tree, left), op.to_string(), equation_generator(monkey_tree, right), ")".to_string()].join(" ");
     }
 
     unreachable!();
@@ -111,38 +111,35 @@ fn part1(input: &HashMap<String, Monkey>) -> Output {
     tree_walk(&input, &start)
 }
 
-fn part2(input: &HashMap<String, Monkey>) -> i64{
-    let start = String::from("root");
+fn part2(input: &HashMap<String, Monkey>) -> f64{
     let mut input = input.clone();
-    input.entry("root".to_string()).and_modify(|job| {
-        if let Monkey::Job(m1, m2, op) = job {
-            *job = Monkey::Job(m1.clone(), m2.clone(), '=');
-        }
-    });
+    let (left, right) = &input.get(&"root".to_string()).and_then(|job| {
+        if let Monkey::Job(m1, m2, _) = job {
+            return Some((m1.clone(), m2.clone()));
+        } else {
+            return None;
+        };
+    }).unwrap();
 
+    let result = tree_walk(&input, &right);
 
-    for i in 3_876_907_167_495..3_876_907_167_496{
+    let mut low = 0.0;
+    let mut high = 10_000_000_000_000.0;
+
+    while low < high {
+        let pivot = ((low + high) / 2.0_f64).round();
         input.entry("humn".to_string()).and_modify(|job| {
-            if let Monkey::Num(n) = job {
-                *job = Monkey::Num(i);
+            if let Monkey::Num(_) = job {
+                *job = Monkey::Num(pivot);
             }
         });
-        if tree_walk(&input, &start) == 0 {
-            return i
+        match result.total_cmp(&tree_walk(&input, &left)) {
+            std::cmp::Ordering::Less => low = pivot,
+            std::cmp::Ordering::Equal => return pivot,
+            std::cmp::Ordering::Greater => high = pivot,
         }
     }
-    -1
-    /*
-    let mut input = input.clone();
-    input.entry("root".to_string()).and_modify(|job| {
-        if let Monkey::Job(m1, m2, op) = job {
-            *job = Monkey::Job(m1.clone(), m2.clone(), '=');
-        }
-    });
-    let start = String::from("root");
-    println!("{}", equation_generator(&input, &start));
-    0
-    */
+    unreachable!();
 }
 
 #[cfg(test)]
@@ -152,12 +149,12 @@ mod tests {
     #[test]
     fn day0_part1_output() {
         let input = parse_input(get_input());
-        assert_eq!(24947355373338, part1(&input));
+        assert_eq!(24947355373338.0, part1(&input));
     }
 
     #[test]
     fn day0_part2_output() {
         let input = parse_input(get_input());
-        assert_eq!(3876907167495, part2(&input));
+        assert_eq!(3876907167495.0, part2(&input));
     }
 }
